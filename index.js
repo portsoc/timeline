ec096baa2d7ad77302e385668243424b1a49fbbb
Jacek Kopecky <jacek@jacek.cz>
2020-11-05 09:49:42 +0000

    :shirt:

--- index.js
+++ index.js
@@ -2,20 +2,20 @@
 // page for safe shorthand use
 
 const UNIT = 24;
-const DAY_MILLIS = 1000*60*60*24;
+const DAY_MILLIS = 1000 * 60 * 60 * 24;
 const tasks = [];
 const keyDates = [];
 const el = {};
 
-let vert_offset = 300; // todo auto adjust this
+const vertOffset = 300; // todo auto adjust this
 
 window.addEventListener('load', init);
 
 function init() {
-  document.querySelectorAll("[id]").forEach( e => el[e.id] = e );
-  el.datefrom.addEventListener("change", redraw);
-  el.dateto.addEventListener("change", redraw);
-  el.daywidth.addEventListener("input", redraw);
+  document.querySelectorAll('[id]').forEach(e => { el[e.id] = e; });
+  el.datefrom.addEventListener('change', redraw);
+  el.dateto.addEventListener('change', redraw);
+  el.daywidth.addEventListener('input', redraw);
 
   addDummyTasks(el.datefrom.valueAsDate, el.dateto.valueAsDate);
   addDummyKeyDates(el.datefrom.valueAsDate, el.dateto.valueAsDate);
@@ -28,18 +28,18 @@ function redraw() {
   const endDate = el.dateto.valueAsDate;
 
   const firstDate = new Date(startDate.getFullYear(), startDate.getMonth(), 1);
-  const lastDate = new Date(new Date(endDate.getFullYear(), endDate.getMonth()+1, 1)-24*60*60*1000);
+  const lastDate = new Date(new Date(endDate.getFullYear(), endDate.getMonth() + 1, 1) - 24 * 60 * 60 * 1000);
 
-  const dayWidth = el.daywidth.value/4;
+  const dayWidth = el.daywidth.value / 4;
 
   const days = (lastDate - firstDate) / 1000 / 24 / 60 / 60 * dayWidth;
   const barHeight = UNIT * 2;
-  const halfBarHeight = UNIT ;
+  const halfBarHeight = UNIT;
   const taskHeight = UNIT;
   const offset = UNIT * 0.6;
   const pad = UNIT * 0.1;
-  
-  document.documentElement.style.setProperty('--unit', UNIT + "px");
+
+  document.documentElement.style.setProperty('--unit', UNIT + 'px');
 
   const width = days + 20 * UNIT;
   const height = 20 * UNIT;
@@ -49,24 +49,24 @@ function redraw() {
   el.timeline = svg('svg', {
     width,
     height,
-    viewBox: `0 0 ${width} ${height}`
+    viewBox: `0 0 ${width} ${height}`,
   });
-   el.timelinecontainer.append(el.timeline);
-  
+  el.timelinecontainer.append(el.timeline);
+
   const barEl = svg('path', {
     id: 'bar',
-    d: `M0,${vert_offset} L${days},${vert_offset} L${days+halfBarHeight},${vert_offset+halfBarHeight} L${days},${vert_offset+barHeight} L0,${vert_offset+barHeight} z`,
+    d: `M0,${vertOffset} L${days},${vertOffset} L${days + halfBarHeight},${vertOffset + halfBarHeight} L${days},${vertOffset + barHeight} L0,${vertOffset + barHeight} z`,
   });
   el.timeline.append(barEl);
 
-  const MONTHS = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+  const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 
   for (let monthStart = firstDate; monthStart < lastDate; monthStart = getNextMonthStart(monthStart)) {
     const text = MONTHS[monthStart.getMonth()];
     const monthMarker = svg('text', {
-      transform: `translate(${dateX(monthStart)+offset},${vert_offset+barHeight-pad}) rotate(-45)`,
-      class: 'month-marker'
+      transform: `translate(${dateX(monthStart) + offset},${vertOffset + barHeight - pad}) rotate(-45)`,
+      class: 'month-marker',
     });
     // todo reconsider 15 above so dates align with months somehow
     monthMarker.textContent = text;
@@ -83,10 +83,10 @@ function redraw() {
 
 
   function drawTask(task) {
-    const y = task.layer * taskHeight + vert_offset + barHeight + halfBarHeight;
+    const y = task.layer * taskHeight + vertOffset + barHeight + halfBarHeight;
     const x = dateX(task.start);
     const width = dateX(task.end) - x;
-    const g = svg('g', {class: 'task'});
+    const g = svg('g', { class: 'task' });
     const barEl = svg('rect', {
       class: 'rect',
       x,
@@ -96,36 +96,34 @@ function redraw() {
       rx: 4,
       fill: task.bg,
     });
-    const textEl = svg('text', {x: x+pad, y: y+taskHeight/2+pad, style: `fill: ${task.color}`});
-    textEl.textContent = task.name; 
+    const textEl = svg('text', { x: x + pad, y: y + taskHeight / 2 + pad, style: `fill: ${task.color}` });
+    textEl.textContent = task.name;
     g.append(barEl, textEl);
     el.timeline.append(g);
   }
 
   function drawKeyDate(keyDate) {
-    const y = vert_offset;
+    const y = vertOffset;
     const x1 = dateX(keyDate.date);
-    const g = svg('g', {class: 'key-date'});
+    const g = svg('g', { class: 'key-date' });
     const style = keyDate.color ? `fill: ${keyDate.color}` : '';
 
-    const dateEl = svg('text', {x: x1, y: y-taskHeight, style, class: 'date'});
-    dateEl.textContent = keyDate.date.getDate(); 
-    
+    const dateEl = svg('text', { x: x1, y: y - taskHeight, style, class: 'date' });
+    dateEl.textContent = keyDate.date.getDate();
+
     const nameEl = svg('text', {
-      transform: `translate(${x1+UNIT/2},${y-taskHeight*2}) rotate(-45)`,
+      transform: `translate(${x1 + UNIT / 2},${y - taskHeight * 2}) rotate(-45)`,
       style,
     });
-    nameEl.textContent = keyDate.name; 
-    
+    nameEl.textContent = keyDate.name;
+
     g.append(dateEl, nameEl);
     el.timeline.append(g);
   }
 
   function dateX(date) {
-    return dayDiff(firstDate, date)*dayWidth;
+    return dayDiff(firstDate, date) * dayWidth;
   }
-
-
 }
 
 
@@ -134,21 +132,19 @@ function dayDiff(date1, date2) {
 }
 
 function getNextMonthStart(date) {
-  return new Date(date.getFullYear(), date.getMonth()+1, 1);
+  return new Date(date.getFullYear(), date.getMonth() + 1, 1);
 }
 
 /*
-  * create a DOM element
-  * options can contain `id`, `classes`
+  * create an SVG DOM element
   */
-function svg(name, attributes = {}, classes = '') {
+function svg(name, attributes = {}) {
   const el = document.createElementNS('http://www.w3.org/2000/svg', name);
   for (const attr of Object.keys(attributes)) {
     el.setAttribute(attr, attributes[attr]);
   }
   return el;
 }
-
 
 
 function addDummyTasks(startDate, endDate) {
@@ -163,7 +159,7 @@ function addDummyTasks(startDate, endDate) {
   tasks.push({
     name: 'two',
     start: new Date(Number(startDate) + 40 * DAY_MILLIS),
-    end: new Date(Number(startDate) + 80  * DAY_MILLIS),
+    end: new Date(Number(startDate) + 80 * DAY_MILLIS),
     bg: '#339966',
     color: '#FFF',
     layer: 1,
